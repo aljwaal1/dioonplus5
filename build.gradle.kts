afa88e78fe7a3509c6238c4406c804d49138f2be
aljwaal1 <yaya15112016@gmail.com>
2026-07-27 14:53:18 +0300

    Use stable Android build tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
-    }
-}
-
 plugins {
-    id("com.android.application") version "9.3.0" apply false
+    id("com.android.application") version "9.3.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
