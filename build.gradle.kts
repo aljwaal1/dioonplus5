db4898a6b33d390f3c6327b2a2442b94ae4c7914
aljwaal1 <yaya15112016@gmail.com>
2026-07-27 14:41:43 +0300

    Configure Android and Compose build

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
+
+plugins {
+    id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+}
