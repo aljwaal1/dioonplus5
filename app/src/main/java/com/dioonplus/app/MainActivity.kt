d8d598b1c0049fc08c572beb277304818a392fc1
aljwaal1 <yaya15112016@gmail.com>
2026-08-09 20:20:03 +0300

    Initialize Google Mobile Ads SDK

--- app/src/main/java/com/dioonplus/app/MainActivity.kt
+++ app/src/main/java/com/dioonplus/app/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.dioonplus.app.ui.theme.DioonPlusTheme
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         enableEdgeToEdge()
+        MobileAds.initialize(this) {}
         setContent {
             DioonPlusTheme {
                 DioonPlusApp()
